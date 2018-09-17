b41462236e79c479175f07fb5fbcc18425fc492c
Smit Patel <smitpatel@users.noreply.github.com>
2018-09-17 15:51:22 -0700

    Add an EF Core Sample app targetting 2.1 packages

---
+++ EFSampleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFSampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new MyContext())
+            {
+                // Recreate database
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                // Seed database
+
+
+                db.SaveChanges();
+            }
+
+            using (var db = new MyContext())
+            {
+                // Run queries
+                var query = db.Blogs.ToList();
+            }
+
+            Console.WriteLine("Program finished.");
+        }
+    }
+
+
+    public class MyContext : DbContext
+    {
+        private static ILoggerFactory LoggerFactory => new LoggerFactory().AddConsole(LogLevel.Trace);
+
+        // Declare DBSets
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Select 1 provider
+            optionsBuilder
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=_ModelApp;Trusted_Connection=True;Connect Timeout=5;ConnectRetryCount=0")
+                //.UseSqlite("filename=_modelApp.db")
+                //.UseInMemoryDatabase(databaseName: "_modelApp")
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(LoggerFactory);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure model
+        }
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+    }
+}
